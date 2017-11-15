ed08ae618409307f7324e8cf3a9b52b1866bcf83
Conjurer Margoss <margy@margoss-retreat.org>
2017-11-15 20:42:40 +0100

    missing changes from 2 last commits

--- gituser.bat
+++ gituser.bat
@@ -1,4 +1,4 @@
-git config user.name Conjurer Margoss
-git config user.email margy@margoss-retreat.org
+git config --local --replace-all user.name "Conjurer Margoss"
+git config --local --replace-all user.email margy@margoss-retreat.org
 
 :: Check https://www.git-tower.com/learn/git/faq/change-author-name-email to change authors of old commits
