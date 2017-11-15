7d3a8e8d4ccbb63ddbb21d857b7fc19b358a48e4
Conjurer <margy@margoss-retreat.org>
2017-11-15 20:32:58 +0100

    set git user script

---
+++ gituser.bat
@@ -0,0 +1,4 @@
+git config user.name Conjurer Margoss
+git config user.email margy@margoss-retreat.org
+
+:: Check https://www.git-tower.com/learn/git/faq/change-author-name-email to change authors of old commits
