3aff8dfa9bc4d1743d27e96158c8bf7c2d5907f6
Conjurer Margoss <margy@margoss-retreat.org>
2017-12-06 11:21:37 +0100

    Remove duplicate enum

--- Input/Keyboard/User32_SendInput_VirtualKeycode.cs
+++ Input/Keyboard/User32_SendInput_VirtualKeycode.cs
@@ -9,12 +9,6 @@ using System.Windows.Forms;
 
 namespace SpongeBot.Input.Keyboard
 {
-    /// <summary>
-    /// List of Virtual Key Codes
-    /// http://kbdedit.com/manual/low_level_vk_list.html
-    /// http://cherrytree.at/misc/vk.htm
-    /// https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx?f=255&MSPPError=-2147217396
-    /// </summary>
     class User32_SendInput_VirtualKeycode : User32_SendInput, ISendString
     {
         public void SendString(string toType)
