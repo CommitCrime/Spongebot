0bb83be9bf99f26bfb98c59cd587a69b5d664b0e
Conjurer Margoss <margy@margoss-retreat.org>
2017-12-06 10:40:43 +0100

    links for keycodes

--- Input/Keyboard/User32_SendInput_VirtualKeycode.cs
+++ Input/Keyboard/User32_SendInput_VirtualKeycode.cs
@@ -10,7 +10,12 @@ using System.Windows.Forms;
 
 namespace SpongeBot.Input.Keyboard
 {
-    //Does nto work
+    /// <summary>
+    /// List of Virtual Key Codes
+    /// http://kbdedit.com/manual/low_level_vk_list.html
+    /// http://cherrytree.at/misc/vk.htm
+    /// https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx?f=255&MSPPError=-2147217396
+    /// </summary>
     class User32_SendInput_VirtualKeycode : User32_SendInput, ISendString
     {
         public void SendString(string toType)
